98eb6f3e3f157337f6f7a598b86f0b2da4781960
VIDI <vidi@infovis.cs.ucdavis.edu>
2023-10-20 21:01:39 -0700

    prepping  backend

--- backend/services/gunicorn/app/__init__.py
+++ backend/services/gunicorn/app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
+import os
+
+proj_path = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -13,3 +16,26 @@ app.url_map.strict_slashes = False
 def hello_world():
     print("Hello Geospatial")
     return "Hello Geospatial"
+
+@app.route("/test")
+def test():
+    print("In test")# Get the path of current working directory 
+    path = os.getcwd() 
+    
+    # Get the list of all files and directories 
+    # in current working directory 
+    dir_list = os.listdir(path) 
+    
+    
+    print("Files and directories in '", path, "' :")  
+    # print the list 
+    print(dir_list)
+    return dir_list
+
+@app.route("/difference")
+def difference():
+    print("SDFSDF")
+    with open(proj_path + "data/difference_unmetdemand.json") as f:
+        data = json.load(f)
+    print(data)
+    return data
